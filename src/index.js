9880d7ff17eed6ed8844ad2c5887d59a4ac5c80d
hxingzhao <584693681@qq.com>
2018-05-07 00:06:35 +0800

    加载字体文件

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import Icon from './sp.png'
 import './style.css'
 function component() {
     var element = document.createElement('div');
-
+    var span = document.createElement('span');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');    
     var myIcon = new Image();
     myIcon.src = Icon;
+    span.classList.add('iconfont','t-daijiejue');
     element.appendChild(myIcon);
+    element.appendChild(span);
     element.classList.add('a');
     return element;
 }
