1a370bf024ea6450eb7f78c8bf4f8c63b8952d30
hxingzhao <584693681@qq.com>
2018-05-06 22:19:10 +0800

    上一次合并时有文件未合并，这次补充

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import _ from 'lodash';
-<<<<<<< HEAD
 import Icon from './sp.png'
-=======
 import './style.css'
->>>>>>> css-loader
 function component() {
     var element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');    
-<<<<<<< HEAD
     var myIcon = new Image();
     myIcon.src = Icon;
     element.appendChild(myIcon);
-=======
     element.classList.add('a');
->>>>>>> css-loader
     return element;
 }
 
