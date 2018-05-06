f61d083a5355085245cb6d9583e616fd382a8a69
hxingzhao <584693681@qq.com>
2018-05-06 21:04:48 +0800

    file-loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import _ from 'lodash';
+import Icon from './sp.png'
 function component() {
     var element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');    
-
+    var myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,15 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
     }
 };
