3af5ad4451fa3f9bc729c4c6f21682dc18960387
hxingzhao <584693681@qq.com>
2018-05-10 23:23:32 +0800

    添加dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,11 @@ module.exports = {
     },
     // 每个模块相互分离，并用模块名称进行注释。你会看到转译之前的代码，正如编写它时。这取决于 loader 支持。
     devtool: 'inline-source-map',
+    // 提供了一个简单的 web 服务器，并且能够实时重新加载(live reloading)
+    devServer: {
+        // 以上配置告知 webpack-dev-server，在 localhost:8080 下建立服务，将 dist 目录下的文件，作为可访问文件。
+        contentBase: './dist'
+    },
     plugins: [
         // HtmlWebpackPlugin 创建了一个全新的文件，所有的 bundle 会自动添加到 html 中。
         new HtmlWebpackPlugin({
