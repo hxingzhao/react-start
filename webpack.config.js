85b377d596df0f86a24f0acc568670b93daaf868
hxingzhao <584693681@qq.com>
2018-05-10 19:24:14 +0800

    添加source map

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,8 @@ module.exports = {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    // 每个模块相互分离，并用模块名称进行注释。你会看到转译之前的代码，正如编写它时。这取决于 loader 支持。
+    devtool: 'inline-source-map',
     plugins: [
         // HtmlWebpackPlugin 创建了一个全新的文件，所有的 bundle 会自动添加到 html 中。
         new HtmlWebpackPlugin({
